0fbd647088e3700cde07a315ea8b7cdd7fed1cc4
OgurecRoman <treninasonya@yandex.ru>
2024-10-04 02:11:15 +0300

    add main screen, design

--- app/src/main/java/com/example/job_search/MainActivity.kt
+++ app/src/main/java/com/example/job_search/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.job_search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nav_view: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(this, R.id.my_nav_graph)
+
+        setupWithNavController(nav_view, navController)
     }
 }
